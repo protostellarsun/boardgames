2693a1f896fba50004aad358aa4c641bc725e843
Yue Ru Sun <yrsun@zenefits.com>
2018-10-09 12:17:29 -0700

    init index page

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
 ReactDOM.render(
   React.createElement("div", null, "Hello World"),
