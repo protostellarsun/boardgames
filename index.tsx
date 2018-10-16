0ed4a564d2b58d6ba930f47380de63c0f1d6ed2f
Yue Ru Sun <yrsun@zenefits.com>
2018-10-15 17:13:50 -0700

    tweak tslint and babel

--- index.tsx
+++ index.tsx
@@ -1,11 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 const HelloWorld = () => {
-  return <div>Hello World</div>;
+    return <div>Hello World</div>;
 };
 
-ReactDOM.render(
-    <HelloWorld/>,
-    document.getElementById("root")
-);
+ReactDOM.render(<HelloWorld />, document.getElementById('root'));
